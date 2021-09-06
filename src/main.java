9a2dce91c37b2bf16af4d9537baa8841093121a8
YunRuiHuang <yunruihuang@gmail.com>
2021-09-06 17:22:24 -0400

    ignore out?

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args){
 
         System.out.println("hello World");
-        System.out.println("Chenge on the  laptop");
+        System.out.println("Change on the  laptop");
     }
 
 
